6f02048f19a348b4254071a2b014631c5e682345
Mufan Liu <77792892+Evan-sudo@users.noreply.github.com>
2025-12-09 22:36:58 +0800

    Update press.py

--- arguments/dsnerf/press.py
+++ arguments/dsnerf/press.py
@@ -4,7 +4,7 @@ ModelHiddenParams = dict(
      'grid_dimensions': 2,
      'input_coordinate_dim': 4,
      'output_coordinate_dim': 16,
-     'resolution': [32, 32, 32, 150]
+     'resolution': [32, 32, 32, 76]
     },
 )
 OptimizationParams = dict(
