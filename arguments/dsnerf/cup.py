1cd6a66afc6c55fea7e160206b3844f8cba50041
Mufan Liu <77792892+Evan-sudo@users.noreply.github.com>
2025-12-09 22:36:04 +0800

    Update cup.py

--- arguments/dsnerf/cup.py
+++ arguments/dsnerf/cup.py
@@ -4,7 +4,7 @@ ModelParams=dict(
      'grid_dimensions': 2,
      'input_coordinate_dim': 4,
      'output_coordinate_dim': 16,
-     'resolution': [32, 32, 32, 200]  #513
+     'resolution': [32, 32, 32, 100]  #513
     },
     defor_depth = 3,
 )
