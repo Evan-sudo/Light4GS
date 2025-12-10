2a59e79e18e19153d2febb6f5fbddc1a73589b78
Mufan Liu <77792892+Evan-sudo@users.noreply.github.com>
2025-12-10 11:09:50 +0800

    Update train_nerfds.sh

--- train_nerfds.sh
+++ train_nerfds.sh
@@ -3,6 +3,6 @@
 python train.py \
   -s /home/old/gaussian4d/workspace/4DGaussians/data/nerf-ds/press \
   --port 6017 \
-  --expname "dsnerf/press" \
+  --expname "nerf-ds/press" \
   --configs arguments/dsnerf/press.py \
   --rlambda 0.001
